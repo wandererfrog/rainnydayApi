3ea7f023e260c4e243c8846442ab9f966935905f
Diogo Afonso Barroso <diogo.barroso@protonmail.com>
2020-01-24 22:10:08 +0100

    Server port config through ENV

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const {searchCityName,getWeatherData} = require('./src/routes.js')
 const app = new Koa();
 const router = new Router();
 
+const port = process.env.PORT
+
 router.get('/v1/search', searchCityName);
 
 // router.get('/v1/weather/',getWeather);
@@ -17,5 +19,5 @@ app.use(cors({ options : {origin : '*'} }))
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.info("Starting server at port: 5000")
-app.listen(5000);
+console.info("Starting server at port:"+port);
+app.listen(port);
